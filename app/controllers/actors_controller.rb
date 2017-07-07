1194daa35a62388e658fe12c0d77e28ac3855234
Diego Camacho <hackvan@gmail.com>
2017-07-07 14:42:41 -0500

    agregada la funcionalidad para crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,22 @@
 class ActorsController < ApplicationController
-   def show
-     @actors = Actor.all
-   end
+  def new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to action: "show"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @actors = Actor.all
+  end
+
+  private
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url)
+  end
 end
